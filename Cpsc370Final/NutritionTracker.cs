718255b022ba96acaa2a6a4da802914fa205de3a
Connor Zermeno <czermeno@chapman.edu>
2025-01-29 21:46:47 -0800

    Running the Food and Calorie Menu

--- Cpsc370Final/NutritionTracker.cs
+++ Cpsc370Final/NutritionTracker.cs
@@ -2,5 +2,41 @@
 
 public class NutritionTracker
 {
-    
+    private readonly FoodDatabase _foodDatabase = new();
+    private readonly FoodLog _foodLog = new();
+    private readonly UserInterface _ui = new();
+    private int _calorieGoal = 0;
+
+    public void Run()
+    {
+        bool running = true;
+        while (running)
+        {
+            _ui.DisplayMenu();
+            string choice = _ui.GetUserInput("");
+
+            switch (choice)
+            {
+                case "1":
+                    AddFoodItem();
+                    break;
+                case "2":
+                    _foodLog.DisplayLog();
+                    break;
+                case "3":
+                    DisplayTotalCalories();
+                    break;
+                case "4":
+                    SetCalorieGoal();
+                    break;
+                case "5":
+                    Console.WriteLine("Exiting program...");
+                    running = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Try again.");
+                    break;
+            }
+        }
+    }
 }
