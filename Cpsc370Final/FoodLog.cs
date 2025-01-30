ca387500e4e3e6e17e6c71543fbec71dd2844419
Connor Zermeno <czermeno@chapman.edu>
2025-01-29 21:24:45 -0800

    Calculating Total Amount of Calories

--- Cpsc370Final/FoodLog.cs
+++ Cpsc370Final/FoodLog.cs
@@ -2,5 +2,23 @@
 
 public class FoodLog
 {
-    
+    public int CalculateTotalCalories()
+    {
+        int totalCalories = 0;
+
+        foreach (string entry in _logEntries)
+        {
+            string[] parts = entry.Split('-');
+            if (parts.Length >= 3))
+            {
+                string caloriePart = parts[2].Replace(" kcal", "").Trim();
+                if (int.TryParse(caloriePart, out int calories))
+                {
+                    totalCalories += calories;
+                }
+            }
+        }
+        
+        return totalCalories;
+    }
 }
