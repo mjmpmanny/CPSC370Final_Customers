518ce3eb4c3923183b5b290d3ed1be386e5a9d14
kurtLim41 <kulim@chapman.edu>
2025-01-29 20:28:21 -0800

    created the food item class

--- Cpsc370Final/FoodItem.cs
+++ Cpsc370Final/FoodItem.cs
@@ -2,5 +2,13 @@
 
 public class FoodItem
 {
-    
+    public string Name { get; }
+    public int CaloriesPer100G { get; }
+
+
+    public FoodItem(string name, int calories)
+    {
+        Name = name;
+        CaloriesPer100G = calories;
+    }
 }
